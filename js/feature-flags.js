0c7bc025146f5eb598fe58f34c97a668fa30c4a8
bkseatown <bkseatown@gmail.com>
2026-02-27 16:44:51 +0800

    Re-enable Writing Studio header tab

--- js/feature-flags.js
+++ js/feature-flags.js
@@ -3,7 +3,7 @@
 
   // Flip this to true when you are ready to share Writing Studio.
   const flags = {
-    writingStudio: false
+    writingStudio: true
   };
 
   window.WQFeatureFlags = Object.freeze(flags);
